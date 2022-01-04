476587269527dc267325bca1645a90d66773fa83
LaChance <miharisoababef@gmail.com>
2022-01-04 14:40:59 +0100

    Ajout des cartes de compétences

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,21 +10,23 @@
 
     <v-row class="ma-12 ">
       <v-col>
-        <v-card elevation="0" class="ma-12" max-width="500">
-        <v-card-title style="font-size:20px"> <h1> Bienvenu !</h1> </v-card-title>
-          <v-card-text style="font-size:17px"> Etudiant au lycée VHB en BTS SIO depuis bientôt 2 ans !<br>
+        <v-card elevation="0" class="ma-12 text-md-left " max-width="800">
+        <v-card-title style="font-size:30px"> <h1> Bienvenu !</h1> </v-card-title>
+          <v-card-text style="font-size:28px; line-height:1.2em" > Je suis étudiant au Lycée Victor-et-Hélène-Basch (VHB) en BTS SIO depuis 2 ans.<br>
+          <br>Je continue à développer et à travailler mes compétences tout au long de ma formation et en dehors de celle-ci <br>
 
-          <br>Vous pouvez consultez mes Compétences
+          <br>Vous pouvez consultez mes productions 
           </v-card-text>
       
-          <v-btn color="#CFA0E9" fab small dark class="ma-3" @click:>
-            <v-icon>mdi-account</v-icon>
+          
+          <v-btn color="#5000ca" fab small dark class="ma-3"  x-large @click:>
+            <v-icon>mdi-folder-multiple</v-icon>
           </v-btn>
         </v-card>
       </v-col>
      
       <v-col class="ml-26">
-        <v-img  max-height="380" max-width="500" src="../assets/pouce.png" id="pouce" @load="go" ></v-img>
+        <v-img  max-height="459" max-width="500" src="../assets/coucou.png" id="pouce" @load="go" ></v-img>
       </v-col>
     </v-row>
     
@@ -46,8 +48,13 @@
       go(){
         anime({
           targets: '#pouce',
-          translateX: 250,
-          duration: 4000,
+          translateX: [250, 250],
+          scale: 1,
+          rotate: '0.05turn',
+          direction:'alternate',
+          loop: true,
+          duration:'1200'
+
         })}
     }
 
--- src/views/Skill.vue
+++ src/views/Skill.vue
@@ -7,12 +7,49 @@
         </h1>
       </v-col>
     </v-row>
+
     <v-row class="ma-12">
-        <v-card>
-            <v-card-title >
-                test
-            </v-card-title >
+      <v-col class="ml-12">
+        <v-card elevation="2" class="ma-12" max-width="300">
+          <v-card-title style="font-size:20px" > <v-icon color="#5000ca"> mdi-desktop-mac </v-icon> <span style="font-weight:bold" color="#5000ca" class="ml-2"> Les outils</span>  </v-card-title>
+            <v-card-text style="font-size:17px"> 
+            - Visual Studio Code <br>
+            - Git <br>
+            - GLPI<br>
+            - PGI <br>
+            </v-card-text>
+        
+            <v-btn color="#5000ca" rounded elevation="0" dark class="ma-3" @click:>
+              <v-icon>mdi-folder-multiple</v-icon>
+            </v-btn>
         </v-card>
+      </v-col>
+      <v-col> <!-- Développement -->
+        <v-card elevation="2" class="ma-12" max-width="300">
+          <v-card-title style="font-size:20px">  <v-icon color="#5000ca"> mdi-xml </v-icon> </v-card-title>
+            <v-card-text style="font-size:17px"> Etudiant au lycée VHB en BTS SIO depuis bientôt 2 ans !<br>
+
+            <br>Vous pouvez consultez mes Compétences
+            </v-card-text>
+        
+            <v-btn color="#5000ca" rounded elevation="0" dark class="ma-3" @click:>
+              <v-icon>mdi-folder-multiple</v-icon>
+            </v-btn>
+        </v-card>
+      </v-col>
+      <v-col> <!-- Développement -->
+        <v-card elevation="2" class="ma-12" max-width="300">
+          <v-card-title style="font-size:20px">  <v-icon color="#5000ca"> mdi-console </v-icon> </v-card-title>
+            <v-card-text style="font-size:17px"> Je suis étudiant au lycée VHB en BTS SIO depuis bientôt 2 ans<br>
+
+            <br>Vous pouvez consultez mes productions
+            </v-card-text>
+        
+            <v-btn color="#5000ca" rounded elevation="0" dark class="ma-3" @click:>
+              <v-icon>mdi-folder-multiple</v-icon>
+            </v-btn>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
